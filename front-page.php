fc7b9dbb099e5435bdf103611ddfbbfc647c57c8
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-25 21:16:56 -0230

    add: middle2

--- front-page.php
+++ front-page.php
@@ -2,6 +2,7 @@
     get_header();
     get_template_part("/partials/front-page/banner");
     get_template_part("/partials/front-page/middle1");
+    get_template_part("/partials/front-page/middle2");
     get_template_part("/partials/front-page/main");
     get_template_part("/partials/front-page/bottom1");
     get_footer();
---
+++ partials/front-page/middle2.php
@@ -0,0 +1,32 @@
+<div class="main-box bg-light-blue lg:py-24 md:py-20 py-10">
+    <div class="wrapper">
+        <div class="lg:flex gap-10 w-full text-center lg:text-left">
+            <h2 class="mb-5 lg:mb-0 lg:text-h3 md:text-h4 text-h5 text-light-black">We are building software solution that solves your business challenges</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.</p>
+        </div>
+
+        <div class="lg:mt-16 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          <div class="lg:p-10 p-5 bg-white rounded-[20px] w-full text-center lg:text-left md:max-w-[500px] lg:max-w-full">
+            <div class="flex items-center justify-center lg:justify-start">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon1.svg'); ?>
+            </div>
+            <h2 class="mt-8 mb-4 text-h5 leading-[1.065] text-light-black">Invoicing</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, </p>
+          </div>
+          <div class="lg:p-10 p-5 bg-white rounded-[20px] w-full text-center lg:text-left md:max-w-[500px] lg:max-w-full">
+            <div class="flex items-center justify-center lg:justify-start">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon2.svg'); ?>
+            </div>
+            <h2 class="mt-8 mb-4 text-h5 leading-[1.065] text-light-black">Support</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, </p>
+          </div>
+          <div class="lg:p-10 p-5 bg-white rounded-[20px] w-full text-center lg:text-left md:max-w-[500px] lg:max-w-full lg:col-span-1 md:col-span-2 justify-self-center">
+            <div class="flex items-center justify-center lg:justify-start">
+              <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/middle2-icon3.svg'); ?>
+            </div>
+            <h2 class="mt-8 mb-4 text-h5 leading-[1.065] text-light-black">Surveying</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, </p>
+          </div>
+        </div>
+    </div>
+</div>
