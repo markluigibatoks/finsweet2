9801c9bc06119109599aa9716c2b4c6d878f20fc
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-25 13:44:30 -0230

    add: banner

--- partials/front-page/banner.php
+++ partials/front-page/banner.php
@@ -1,14 +1,31 @@
-<div class="main-box p-0">
-    <div class="w-full h-full">
-        <img class="w-full h-full object-cover" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/placeholder.svg" alt="placeholder"/>
-    </div>
-    <div class="main-box bg-black lg:bg-transparent lg:absolute lg:top-1/2 lg:-translate-y-1/2 lg:left-0">
-        <div class="wrapper">
-            <div class="w-full text-center lg:text-left text-white">
-                <span>Sub heading text-center</span>
-                <h2 class="text-4xl">Lorem Ipsum text</h2>
-                <a href="#" class="button-primary block max-w-max mx-auto lg:mx-0">Click here</a>
+<div class="overflow-hidden main-box lg:pt-28 lg:pb-37 pt-20 bg-light-blue">
+    <div class="wrapper">
+        <div class="z-20 relative lg:max-w-[576px] w-full text-center lg:text-left">
+            <h2 class="mb-4 lg:text-h1 md:text-h4 text-h5 text-secondary">Prosper with our bespoke solutions</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique</p>
+            <div class="mt-8 flex sm:flex-row flex-col gap-5 lg:gap-10 items-center lg:justify-start justify-center">
+                <a href="/services" class="button-primary block">See our services</a>
+                <a href="/services" class="text-secondary font-semibold hover:underline">See All Services</a>
+            </div>
+            
+            <p class="mt-14 mb-4 text-[#394149] opacity-50">Worked with 100+ Companies</p>
+            <div class="flex gap-5 lg:gap-12 items-center justify-center lg:justify-start">
+                <div class="max-w-max">
+                    <img class="w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/banner-img1.png" alt="logo"/>
+                </div>
+
+                <div class="max-w-max">
+                    <img class="w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/banner-img2.png" alt="logo"/>
+                </div>
+
+                <div class="max-w-max">
+                    <img class="w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/banner-img3.png" alt="logo"/>
+                </div>
             </div>
         </div>
+    </div>
+
+    <div class="z-0 lg:absolute bottom-0 lg:-right-25 xl:right-[120px] lg:max-w-[700px] xl:max-w-[934px] xl:w-[calc(60%-100px)] lg:w-full">
+        <img class="w-full h-full object-contain" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/banner.png" alt="finsweet ambassador"/>
     </div>
 </div>
