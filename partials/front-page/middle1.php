e29d038c91457816025d08fe19ae7e7626d2d285
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-25 21:01:22 -0230

    add: middle1

--- partials/front-page/middle1.php
+++ partials/front-page/middle1.php
@@ -1,9 +1,37 @@
-<div class="main-box lg:py-30 py-10">
+<div class="main-box lg:py-24 md:py-20 py-10">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <h2 class="text-4xl">Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+        <div class="lg:max-w-[735px] w-full text-center lg:text-left">
+            <h2 class="lg:mb-7 mb-5 text-h5 lg:text-h2 md:text-h4 text-secondary">We help more than 1500 companies from all sectors</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.</p>
+        </div>
+
+        <div class="lg:mt-16 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            <div class="w-full text-center lg:text-left md:max-w-[500px] lg:max-w-full">
+                <div class="max-w-max mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/middle1-img1.jpg" alt="men and women are smiling"/>
+                </div>
+                <h2 class="md:text-h4 text-h5 lg:text-paragraph-large leading-[1.065] mt-8 lg:mb-4">Business strategy</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, </p>
+                <a href="#" aria-label="dummy-link" class="mt-6 text-primary hover:underline max-w-max mx-auto lg:mx-0 flex gap-2 items-center">Learn More <span class="flex items-center justify-center"><?php echo file_get_contents(get_template_directory() . '/assets/images/arrow-right.svg'); ?> </span></a>
+            </div>
+
+            <div class="w-full text-center lg:text-left md:max-w-[500px] lg:max-w-full">
+                <div class="max-w-max mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/middle1-img2.jpg" alt="men and women are smiling"/>
+                </div>
+                <h2 class="md:text-h4 text-h5 lg:text-paragraph-large leading-[1.065] mt-8 lg:mb-4">Digitalization</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, </p>
+                <a href="#" aria-label="dummy-link" class="mt-6 text-primary hover:underline max-w-max mx-auto lg:mx-0 flex gap-2 items-center">Learn More <span class="flex items-center justify-center"><?php echo file_get_contents(get_template_directory() . '/assets/images/arrow-right.svg'); ?> </span></a>
+            </div>
+
+            <div class="lg:col-span-1 md:col-span-2 justify-self-center w-full text-center lg:text-left md:max-w-[500px] lg:max-w-full">
+                <div class="max-w-max mx-auto">
+                    <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/middle1-img3.jpg" alt="men and women are smiling"/>
+                </div>
+                <h2 class="md:text-h4 text-h5 lg:text-paragraph-large leading-[1.065] mt-8 lg:mb-4">Risk assessment</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, </p>
+                <a href="#" aria-label="dummy-link" class="mt-6 text-primary hover:underline max-w-max mx-auto lg:mx-0 flex gap-2 items-center">Learn More <span class="flex items-center justify-center"><?php echo file_get_contents(get_template_directory() . '/assets/images/arrow-right.svg'); ?> </span></a>
+            </div>
         </div>
     </div>
 </div>
