9a5232b92e6e3c328a6c58cdec3e6988ef933b01
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-26 19:48:59 -0230

    add: front-page/main

--- partials/front-page/main.php
+++ partials/front-page/main.php
@@ -1,9 +1,28 @@
-<main class="main-box lg:py-30 py-10 bg-black text-white">
+<main class="main-box lg:pt-25 lg:pb-20 py-10 bg-white">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <h1 class="text-4xl">Lorem Ipsum</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+        <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-5 lg:gap-12 justify-between">
+            <div class="order-2 lg:order-1 w-full text-center lg:text-left">
+                <h1 class="mb-5 lg:mb-8 lg:text-h2 md:text-h4 text-h5 text-light-black">The energy of a start-up combined with 30 years of experience.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.</p>
+                
+                <div class="mt-12 grid grid-cols-1 sm:grid-cols-2">
+                    <div class="w-full text-center lg:text-left">
+                        <span class="font-inter md:text-4xl text-h5 text-primary">15+</span>
+                        <h2 class="my-4 text-h5 leading-[1.065] text-light-black">Awards received</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.</p>
+                    </div>
+
+                    <div class="w-full text-center lg:text-left gap-6">
+                        <span class="font-inter md:text-4xl text-h5 text-primary">500+</span>
+                        <h2 class="my-4 text-h5 leading-[1.065] text-light-black">Clients served</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="order-1 lg:order-2 max-w-max mx-auto">
+                <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/main-img1.jpg" alt="finsweet2 ambassador"/>
+            </div>
         </div>
     </div>
 </main>
