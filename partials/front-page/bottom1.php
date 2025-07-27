41ef4e5068efb1a1b7469a0719d5a6e01022dbe4
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-27 19:20:09 -0230

    add: front-page/bottom1

--- partials/front-page/bottom1.php
+++ partials/front-page/bottom1.php
@@ -1,9 +1,28 @@
-<div class="main-box lg:py-30 py-10">
+<div class="main-box lg:pb-32 pb-10 bg-white">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <h2 class="text-4xl">Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error, ad totam mollitia similique perferendis numquam commodi perspiciatis provident ipsa amet alias odit labore eaque adipisci aliquam rem dolores sed.</p>
-            <a href="#" class="button-primary mx-auto block mt-5 max-w-max">Click here</a>
+        <div class="md:p-12 p-5 bg-light-blue rounded-[20px]">
+            <div class="overflow-auto flex gap-6 items-center">
+                <button type="button" class="button-tertiary cursor-pointer">Business strategy</button>
+
+                <button type="button" class="button-tertiary inactive cursor-pointer">Digitalization</button>
+
+                <button type="button" class="button-tertiary inactive cursor-pointer">Risk assessment</button>
+            </div>
+
+            <div class="mt-10 lg:mt-20 flex flex-col lg:flex-row gap-5 justify-between items-center w-full lg:text-left text-center">
+                <div class="lg:order-1 order-2 max-w-[572px]">
+                    <h2 class="text-secondary mb-4 text-h5 lg:text-h2 md:text-h4">Helping clients with research and strategy for their business</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque. 
+                    <br />
+                    Mattis purus. Vivamus commodo suscipit tellus et pellent. Curabitur sit amet eros blan esque.
+                    </p>
+                    <a href="#" aria-label="dummy-link" class="md:mt-10 mt-5 text-primary hover:underline max-w-max mx-auto lg:mx-0 flex gap-2 items-center">See all services <span class="flex items-center justify-center"><?php echo file_get_contents(get_template_directory() . '/assets/images/arrow-right.svg'); ?> </span></a>
+                </div>
+
+                <div class="lg:order-2 order-1 w-full max-w-max">
+                    <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom1-img1.jpg" alt="people are brainstorming"/>
+                </div>
+            </div>
         </div>
     </div>
 </div>
