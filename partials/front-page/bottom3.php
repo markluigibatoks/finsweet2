7ddee8883206055ab5a1d1b818b62fba1d19378d
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-28 19:21:47 -0230

    add: bottom3

---
+++ partials/front-page/bottom3.php
@@ -0,0 +1,29 @@
+<div class="main-box bg-white py-10 lg:py-32">
+    <div class="wrapper">
+      <div class="flex gap-5 flex-col lg:flex-row items-center justify-between">
+        <div class="lg:order-1 order-2 lg:max-w-[550px] w-full text-center lg:text-left">
+            <h2 class="lg:mb-10 mb-5 text-secondary lg:text-h2 md:text-h4 text-h5">Finsweet was a dream to work with</h2>
+            <p>Maecenas efficitur scelerisque lorem, et varius lacus tincidunt vel. Pellentesque a arcu vitae diam dapibus mattis vel vel orci. Vivamus eleifend nec felis vel auctor.</p>
+            
+            <div class="mt-10 w-full flex gap-6 items-center justify-center lg:justify-start text-center lg:text-left">
+                <div class="max-w-max">
+                  <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img2.png" alt="chikelu neo"/>
+                </div>
+
+                <div>
+                  <h2 class="text-h5 font-medium leading-[1.71] tracking-normal">Chikelu Neo</h2>
+                  <span class="text-primary text-h6 font-medium leading-[1.71] tracking-normal">CEO at MazeAI</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="lg:order-2 order-1 relative max-w-max">
+          <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/front-page/bottom3-img1.jpg" alt="a video thumbnail"/>
+
+          <button type="button" class="cursor-pointer absolute bottom-5 sm:bottom-10 left-5 sm:left-10 flex gap-4 items-center text-white hover:opacity-90 text-h6 leading-[1.018] hover:text-underline"><span class="font-normal leading-0 tracking-normal text-primary flex items-center justify-center w-13 h-13 bg-white rounded-full">
+          <?php echo file_get_contents(get_template_directory() . '/assets/images/front-page/bottom3-icon1.svg'); ?>
+          </span> Play Video</button>
+        </div>
+      </div>
+    </div>
+</div>
