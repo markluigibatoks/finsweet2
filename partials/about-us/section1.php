c2a75075f230e10224d91d1a84d0b446eac802ca
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-29 21:19:23 -0230

    add: about-us/section1

--- partials/about-us/section1.php
+++ partials/about-us/section1.php
@@ -1,10 +1,34 @@
 <div class="main-box lg:py-32 py-10">
     <div class="wrapper">
-        <div class="w-full text-center">
-            <span>Subheading</span>
-            <h2>About Us</h2>
-            <p>We are a company that values excellence and innovation.</p>
-            <a href="#" class="button-primary mx-auto lg:mx-0">Learn More</a>
+        <div class="lg:flex justify-between gap-10 w-full text-center lg:text-left">
+            <h2 class="text-secondary lg:max-w-[584px] text-h3 md:text-h4 text-h5">The energy of a start-up combined with 30 years of experience</h2>
+            <p class="lg:max-w-[620px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.</p>
+        </div>
+
+        <div class="mt-14 grid grid-cols-1 gap-5 xl:gap-16 sm:grid-cols-2 lg:grid-cols-4">
+            <div class="w-full text-center lg:text-left">
+                <span class="font-inter md:text-4xl text-h5 text-primary">15+</span>
+                <h2 class="my-4 text-h5 leading-[1.065] text-light-black">Awards received</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.</p>
+            </div>
+
+            <div class="w-full text-center lg:text-left gap-6">
+                <span class="font-inter md:text-4xl text-h5 text-primary">500+</span>
+                <h2 class="my-4 text-h5 leading-[1.065] text-light-black">Clients served</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.</p>
+            </div>
+
+            <div class="w-full text-center lg:text-left gap-6">
+                <span class="font-inter md:text-4xl text-h5 text-primary">34</span>
+                <h2 class="my-4 text-h5 leading-[1.065] text-light-black">Employees</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.</p>
+            </div>
+
+            <div class="w-full text-center lg:text-left gap-6">
+                <span class="font-inter md:text-4xl text-h5 text-primary">130+</span>
+                <h2 class="my-4 text-h5 leading-[1.065] text-light-black">Custom solutions</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscin. Curabitur sit amet eros elit et.</p>
+            </div>
         </div>
     </div>
 </div>
