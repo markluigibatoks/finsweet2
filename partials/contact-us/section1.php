18a1165caf19ac52c87d69d95314cd1b83a088e5
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-01 03:55:06 -0230

    add: page-contact

---
+++ partials/contact-us/section1.php
@@ -0,0 +1,49 @@
+<main class="main-box bg-white lg:pb-32 lg:pt-25 py-10">
+    <div class="wrapper">
+      <div class="flex flex-col lg:flex-row gap-32 lg:gap-14">
+        <div class="lg:max-w-[605px] w-full text-center lg:text-left">
+            <h2 class="lg:text-h2 md:text-h4 text-h5 text-secondary mb-6">Have a question ? Let's get in touch with us.</h2>
+            <p>Fill up the Form  and ou team will get back to within 24 hrs</p>
+
+            <form method="POST" id="contact-us-form" class="mt-10 lg:mt-16">
+              <div class="flex flex-col gap-4">
+                <label>
+                  <input type="text" autocomplete="given-name" name="first_name" class="w-full font-inter font-medium text-base tracking-normal px-8 py-4 rounded-[27px] bg-[#D2DAED2B]" placeholder="First Name"/>
+                </label>
+
+                <label>
+                  <input type="text" autocomplete="family-name" name="last_name" class="w-full font-inter font-medium text-base tracking-normal px-8 py-4 rounded-[27px] bg-[#D2DAED2B]" placeholder="Last Name" />
+                </label>
+
+                <label>
+                  <input type="email" autocomplete="email" name="email" class="w-full font-inter font-medium text-base tracking-normal px-8 py-4 rounded-[27px] bg-[#D2DAED2B]" placeholder="Email address"/>
+                </label>
+
+                <textarea name="message" rows="4" placeholder="Type message" class="w-full font-inter font-medium text-base tracking-normal px-8 py-4 rounded-[27px] bg-[#D2DAED2B]"></textarea>
+              </div>
+
+              <button type="submit" class="mt-8 button-primary cursor-pointer">Submit</button>
+            </form>
+        </div>
+
+        <div class="w-full lg:max-w-[400px] xl:max-w-[620px]">
+          <div class="flex sm:flex-row flex-col lg:justify-between gap-10 lg:gap-5">
+            <div class="max-w-[270px]">
+              <h3 class="mb-4 text-h5 leading-[30px] text-secondary">Location</h3>
+              <address class="not-italic font-medium">DLF Cybercity, Bhubaneswar, India, &52050</address>
+            </div>
+
+            <div class="max-w-[270px]">
+              <h3 class="mb-4 text-h5 leading-[30px] text-secondary">Contact Us</h3>
+              <a href="tel:02079932905" class="hover:underline text-h6 font-medium leading-[30px] tracking-normal">020 7993 2905</a>
+              <a href="mailto:hi@finsweet.com" class="mt-1 hover:underline text-h6 font-medium leading-[30px] tracking-normal">hi@finsweet.com</a>
+            </div>
+          </div>
+
+          <div class="w-full mt-10 lg:mt-29 rounded-[20px] overflow-hidden">
+            <iframe class="w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3740.8202669580437!2d85.80516117639074!3d20.349042381135643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1908e025984c55%3A0xee1fcd1f11e55141!2sDLF%20Cyber%20City!5e0!3m2!1sen!2sca!4v1754028857726!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
--- theme-setup/route-contact-us.php
+++ theme-setup/route-contact-us.php
@@ -11,7 +11,8 @@ function contactUsRoutes() {
 
 function createContactUs($data) {
 
-  $full_name  = sanitize_text_field($data['full_name'] ?? '');
+  $first_name  = sanitize_text_field($data['first_name'] ?? '');
+  $last_name  = sanitize_text_field($data['last_name'] ?? '');
   $email = strtolower(trim(sanitize_email($data['email'] ?? '')));
   $message = strtolower(trim(sanitize_text_field($data['message'] ?? '')));
 
@@ -19,7 +20,7 @@ function createContactUs($data) {
     return new WP_Error('incorrect_email', 'Incorrect Email', ['status' => 400]);
   }
 
-  if (empty($full_name) || empty($email) || empty($message)) {
+  if (empty($first_name) || empty($last_name) || empty($email) || empty($message)) {
     return new WP_Error('missing_fields', 'Required fields missing', ['status' => 400]);
   }
 
@@ -29,7 +30,8 @@ function createContactUs($data) {
       'post_status' => 'pending',
       'post_title' => wp_trim_words($message, 5),
       'meta_input' => array(
-        'full_name' => $full_name,
+        'first_name' => $first_name,
+        'last_name' => $last_name,
         'email' => $email,
         'message' => $message,
       )
