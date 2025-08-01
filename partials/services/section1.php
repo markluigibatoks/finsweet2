825481d969bfd030033cec676d90b475a3d56c8b
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-31 23:11:47 -0230

    add: services/section1

---
+++ partials/services/section1.php
@@ -0,0 +1,81 @@
+<div class="main-box bg-white lg:pt-0 py-10">
+    <div class="wrapper">
+        <div class="grid grid-cols-1 md:grid-cols-2 place-items-center gap-x-10 xl:gap-x-20 md:gap-y-20 lg:gap-y-32 items-center">
+          <div class="relative md:order-1 mb-10 md:my-0 max-w-max mx-auto">
+            <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/services/section1-img1.jpg" alt="a woman is talking through a mic"/>
+
+            <div class="absolute top-[23%] -left-5 2xl:-left-12 flex items-center justify-center">
+              <div class="rounded-lg 2xl:rounded-[20px] bg-primary w-10 2xl:w-24 h-24 mix-blend-multiply"></div>
+            </div>
+          </div>
+          <div class="md:order-2 max-w-[584px] w-full text-center md:text-left">
+            <h2 class="mb-6 lg:text-h2 md:text-h4 text-h5 text-secondary">Business strategy</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.met, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.</p>
+
+            <ul class="mt-6 list-disc list-inside text-left">
+              <li>Cost strategy</li>
+              <li>Differentiated product or service strategy</li>
+              <li>Focus on a niche strategy</li>
+            </ul>
+          </div>
+
+          <div class="relative md:order-4 mb-10 mt-20 md:my-0 max-w-max mx-auto">
+            <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/services/section1-img2.jpg" alt="a woman is drinking coffee while working"/>
+
+            <div class="absolute bottom-[33%] -right-5 2xl:-right-12 flex items-center justify-center">
+              <div class="rounded-lg 2xl:rounded-[20px] bg-primary w-10 2xl:w-24 h-24 mix-blend-multiply"></div>
+            </div>
+          </div>
+          <div class="md:order-3 max-w-[584px] w-full text-center md:text-left">
+            <h2 class="mb-6 lg:text-h2 md:text-h4 text-h5 text-secondary">Digitalization</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.met, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis.</p>
+          </div>
+
+          <div class="relative md:order-5 mb-10 mt-20 md:my-0 max-w-max mx-auto">
+            <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/services/section1-img3.jpg" alt="man looking at his feet while walking"/>
+
+            <div class="absolute top-[39%] -left-5 2xl:-left-12 flex items-center justify-center">
+              <div class="rounded-lg 2xl:rounded-[20px] bg-primary w-10 2xl:w-24 h-24 mix-blend-multiply"></div>
+            </div>
+          </div>
+          <div class="md:order-6 max-w-[584px] w-full text-center md:text-left">
+            <h2 class="mb-6 lg:text-h2 md:text-h4 text-h5 text-secondary">Risk assessment</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.met, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.</p>
+            <ul class="mt-6 list-disc list-inside text-left">
+              <li>Individual risk assessment</li>
+              <li>Systems risk assessment</li>
+              <li>Mathematical conceptualization</li>
+            </ul>
+          </div>
+
+          <div class="relative md:order-8 mb-10 mt-20 md:my-0 max-w-max mx-auto">
+            <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/services/section1-img4.jpg" alt="a person is looking up the tower"/>
+
+            <div class="absolute bottom-[33%] -right-5 2xl:-right-12 flex items-center justify-center">
+              <div class="rounded-lg 2xl:rounded-[20px] bg-primary w-10 2xl:w-24 h-24 mix-blend-multiply"></div>
+            </div>
+          </div>
+          <div class="md:order-7 max-w-[584px] w-full text-center md:text-left">
+            <h2 class="mb-6 lg:text-h2 md:text-h4 text-h5 text-secondary">Artificial intelligence</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.met, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis.</p>
+          </div>
+
+          <div class="relative md:order-9 mb-10 mt-20 md:my-0 max-w-max mx-auto">
+            <img class="rounded-[20px] object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/services/section1-img5.jpg" alt="people having a meeting"/>
+
+            <div class="absolute bottom-[20%] -left-5 2xl:-left-12 flex items-center justify-center">
+              <div class="rounded-lg 2xl:rounded-[20px] bg-primary w-10 2xl:w-24 h-24 mix-blend-multiply"></div>
+            </div>
+          </div>
+          <div class="md:order-10 max-w-[584px] w-full text-center md:text-left">
+            <h2 class="mb-6 lg:text-h2 md:text-h4 text-h5 text-secondary">Smart Contracts</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.met, consectetur adipiscing elit. Aliquam leo odio, sagittis quis ornare quis.</p>
+            <ul class="mt-6 list-disc list-inside text-left">
+              <li>Smart contract implementation</li>
+              <li>Smart legal contracts</li>
+              <li>Basic contract law</li>
+            </ul>
+          </div>
+        </div>
+    </div>
+</div>
